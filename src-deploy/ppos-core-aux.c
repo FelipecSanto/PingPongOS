9d8b34f7c4d9bdb123661466fc4c01787dee21f0
Felipe <felpiscs2017@gmail.com>
2025-05-09 23:13:21 -0300

    Código compilando e executando um dos exemplos

--- src-deploy/ppos-core-aux.c
+++ src-deploy/ppos-core-aux.c
@@ -12,6 +12,91 @@
 // ****************************************************************************
 
 
+/*void ppos_init () {
+    // inicializa o sistema operacional
+    taskMain = malloc (sizeof(task_t));
+    taskExec = taskMain;
+    taskDisp = malloc (sizeof(task_t));
+    freeTask = malloc (sizeof(task_t));
+    readyQueue = malloc (sizeof(task_t));
+    sleepQueue = malloc (sizeof(task_t));
+
+    taskMain->id = 0;
+    taskExec->id = 0;
+    taskDisp->id = 0;
+    freeTask->id = 0;
+    readyQueue->id = 0;
+    sleepQueue->id = 0;
+    taskMain->state = PPOS_TASK_STATE_EXECUTING;
+    taskExec->state = PPOS_TASK_STATE_EXECUTING;
+    taskDisp->state = PPOS_TASK_STATE_EXECUTING;
+    freeTask->state = PPOS_TASK_STATE_EXECUTING;
+    readyQueue->state = PPOS_TASK_STATE_EXECUTING;
+    sleepQueue->state = PPOS_TASK_STATE_EXECUTING;
+    taskMain->next = NULL;
+    taskExec->next = NULL;
+    taskDisp->next = NULL;
+    freeTask->next = NULL;
+    readyQueue->next = NULL;
+    sleepQueue->next = NULL;
+    taskMain->prev = NULL;
+    taskExec->prev = NULL;
+    taskDisp->prev = NULL;
+    freeTask->prev = NULL;
+    readyQueue->prev = NULL;
+    sleepQueue->prev = NULL;
+    taskMain->queue = NULL;
+    taskExec->queue = NULL;
+    taskDisp->queue = NULL;
+    freeTask->queue = NULL;
+    readyQueue->queue = NULL;
+    sleepQueue->queue = NULL;
+    taskMain->joinQueue = NULL;
+    taskExec->joinQueue = NULL;
+    taskDisp->joinQueue = NULL;
+    freeTask->joinQueue = NULL;
+    readyQueue->joinQueue = NULL;
+    sleepQueue->joinQueue = NULL;
+    taskMain->exitCode = 0;
+    taskExec->exitCode = 0;
+    taskDisp->exitCode = 0;
+    freeTask->exitCode = 0;
+    readyQueue->exitCode = 0;
+    sleepQueue->exitCode = 0;
+    taskMain->awakeTime = 0;
+    taskExec->awakeTime = 0;
+    taskDisp->awakeTime = 0;
+    freeTask->awakeTime = 0;
+    readyQueue->awakeTime = 0;
+    sleepQueue->awakeTime = 0;
+    taskMain->custom_data = NULL;
+    taskExec->custom_data = NULL;
+    taskDisp->custom_data = NULL;
+    freeTask->custom_data = NULL;
+    readyQueue->custom_data = NULL;
+    sleepQueue->custom_data = NULL;
+    taskMain->context.uc_stack.ss_sp = malloc(STACKSIZE);
+    taskMain->context.uc_stack.ss_size = STACKSIZE;
+    taskMain->context.uc_stack.ss_flags = 0;
+    taskMain->context.uc_link = 0;
+    
+}*/
+
+
+
+task_t * scheduler() {
+    if (readyQueue != NULL) {
+        return readyQueue;
+    }
+    return NULL;
+}
+
+unsigned int _systemTime;
+
+unsigned int systime () {
+    return _systemTime;
+}
+
 
 void before_ppos_init () {
     // put your customization here
@@ -48,6 +133,7 @@ void before_task_exit () {
 #ifdef DEBUG
     printf("\ntask_exit - BEFORE - [%d]", taskExec->id);
 #endif
+
 }
 
 void after_task_exit () {
